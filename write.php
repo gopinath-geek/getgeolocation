480d24186df9c1f339380fba154a66b34c316640
gopinath-geek <gopinath.geek@outlook.com>
2017-08-14 00:31:23 +0530

    Update write.php

--- write.php
+++ write.php
@@ -4,11 +4,13 @@
   $current_time = date("l jS \of F Y h:i:s A");
 
   $content_to_write = array("time"=>$current_time, "lat"=>$lat, "long"=>$long);
-  $file = 'write.txt';
+  $file = 'user_log.txt';
   // Open the file to get existing content
   $old_text = file_get_contents($file);
   // Append a new person to the file
   $old_text .= "$content_to_write\n";
   // Write the contents back to the file
   file_put_contents($file, $old_text);
+
+  echo "Okay";
 ?>
